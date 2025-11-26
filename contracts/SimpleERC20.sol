7b3bbdd1fc242852be2d621e738c0bea2b00c63f
Souvenir <grabilenko.yana@mail.ru>
2025-11-26 22:44:44 +0300

    update ignore

---
+++ contracts/SimpleERC20.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleERC20 {
+    string _name;                                   //0
+    string _symbol;                                 //1
+    uint8 _decimals;                                //2
+    uint _totalSupply;                              //3
+    mapping (address => uint) _balances;            //4
+    mapping(address owner => mapping(address spender => uint value)) _allowances;
+    address contractOwner;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approve(address indexed from, address indexed to, uint value);
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "only Owner");
+        _;
+    }
+
+    constructor(string memory name_, string memory symbol_) {
+        contractOwner = msg.sender;
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() external virtual view returns(string memory) {
+        return _name;
+    }
+    function symbol() external virtual view returns(string memory) {
+        return _symbol;
+    }
+    function totalSupply() external virtual view returns(uint) {
+        return _totalSupply;
+    }
+    function balanceOf(address account) external virtual view returns(uint) {
+        return _balances[account];
+    }
+    function transfer(address to, uint value) external virtual returns(bool) {
+        _transfer(msg.sender, to, value);
+        emit Transfer(msg.sender, to, value);
+
+        return true;
+    }
+    function transferFrom(address from, address to, uint value) external virtual returns(bool) {
+        require(_allowances[from][msg.sender] >= value, "not enough allowance");
+        _transfer(from, to, value);
+        _approve(from, msg.sender, value, false);
+        emit Transfer(from, to, value);
+
+        return true;
+    }
+    function _transfer(address from, address to, uint value) internal virtual {
+        require(from != address(0), "bad from");
+        require(to != address(0), "bad to");
+        _update(from, to, value);
+    }
+    function allowance(address owner, address spender) external virtual view returns(uint amount) {
+        return _allowances[owner][spender];
+    }
+    function approve(address spender, uint value) external virtual returns(bool){
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+    function _approve(address owner, address spender, uint value) internal virtual {
+        _approve(owner, spender, value, true);
+    }
+    function _approve(address owner, address spender, uint value, bool emitEvent) internal virtual {
+        require(owner != address(0), "bad owner");
+        require(spender != address(0), "bad spender");
+
+        _allowances[owner][spender] = value;
+
+        if(emitEvent) {
+            emit Approve(owner, spender, value);
+        }
+    }
+    function mint(address to, uint value) public virtual onlyOwner{
+        _update(address(0), to, value);
+    }
+    function burn(uint value) external virtual returns(bool){
+        _burn(msg.sender, value);
+        return true;
+    }
+    function burnFrom(address from, uint value) external virtual returns(bool){
+        require(_allowances[from][msg.sender] >= value, "not enough allowance");
+        _burn(from, value);
+        _approve(from, msg.sender, value, false);
+        return true;
+    }
+    function _burn(address from, uint value ) internal virtual {
+        _update(from, address(0), value);
+    }
+    function _update(address from, address to, uint value) internal virtual {
+        if (from == address(0)) {
+            _totalSupply += value;
+        } else {
+            require(_balances[from] >= value, "not enough");
+            _balances[from] -= value;
+        }
+    
+        if (to == address(0)) {
+            _totalSupply -= value;
+        } else {
+            _balances[to] += value;
+        }
+    }
+}
---
+++ contracts/SimpleERC20.t.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleERC20.sol";
+import "forge-std/Test.sol";
+import {stdStorage, StdStorage} from "forge-std/Test.sol";
+
+contract SimpleERC20Test is Test {
+    using stdStorage for StdStorage;
+    SimpleERC20 simpleERC20;
+    string defaultName = "DefName";
+    string defaultSymbol = "DN";
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approve(address indexed from, address indexed to, uint value);
+    address account1 = vm.addr(1);
+    address account2 = vm.addr(2);
+    address account3 = vm.addr(3);
+    uint value = 10000;
+
+    function setUp() public {
+        simpleERC20 = new SimpleERC20(defaultName, defaultSymbol);
+    }
+    function test_name() public view {
+        assertEq(simpleERC20.name(), defaultName);
+    }
+    function test_symbol() public view {
+        assertEq(simpleERC20.symbol(), defaultSymbol);
+    }
+    function test_totalSupply() public {
+        vm.store(address(simpleERC20), bytes32(uint256(3)), bytes32(uint256(value)));
+        assertEq(simpleERC20.totalSupply(), value);
+    }
+    function test_balanceOf() public {
+        deal(address(simpleERC20), account1, value);
+        assertEq(simpleERC20.balanceOf(account1), value);
+    }
+    function test_balanceOf222() public {
+        deal(address(simpleERC20), account1, value);
+        assertEq(simpleERC20.balanceOf(account1), value);
+    }
+    function test_approveWithAllownace() public {
+        deal(address(simpleERC20), account2, value);
+        vm.startPrank(account2);
+        simpleERC20.approve(account3, value);
+        assertEq(simpleERC20.allowance(account2, account3), value);
+    }
+    function test_approveWithSlot() public {
+        uint slot = uint256(keccak256(abi.encode(account2, uint256(5))));
+        bytes32 key = keccak256(abi.encode(account3, slot));
+        vm.store(address(simpleERC20), key, bytes32(value));
+        assertEq(simpleERC20.allowance(account2, account3), value);
+    }
+    function test_approveEmit() public {
+        vm.expectEmit(true,true,false, true);
+        emit Approve(account2, account3, value);
+        test_approveWithAllownace();
+    }
+    function test_allowanceWithApprove() public {
+        vm.startPrank(account2);
+        simpleERC20.approve(account3, value);
+        assertEq(simpleERC20.allowance(account2, account3), value);
+    }
+    function test_allowanceWithSlot( ) public {
+        vm.store(
+            address(simpleERC20), 
+            keccak256(abi.encode(account3, keccak256(abi.encode(account2, uint256(5))))), 
+            bytes32(uint256(value))
+        );
+        assertEq(simpleERC20.allowance(account2, account3), value);
+    }
+    function test_transfer() public {
+        deal(address(simpleERC20), account2, value);
+        vm.prank(account2);
+        simpleERC20.transfer(account3, value);
+        assertEq(simpleERC20.balanceOf(account3), value);
+    }
+    function test_transferEmit() public {
+        vm.expectEmit(true,true,false, true);
+        emit Transfer(account2, account3, value);
+        test_transfer();
+    }
+    function test_mint()public {
+        simpleERC20.mint(account1, value);
+        assertEq(simpleERC20.balanceOf(account1), value);
+    }
+    function test_transferFrom() public {
+        test_mint();
+        
+        vm.prank(account1);
+        simpleERC20.approve(account3, value);
+        assertEq(simpleERC20.allowance(account1, account3), value);
+
+        
+        vm.prank(account3);
+        simpleERC20.transferFrom(account1, account2, value);
+        
+        assertEq(simpleERC20.balanceOf(account2), value);
+    }
+    function test_burn() public {
+        test_mint();
+        assertEq(simpleERC20.balanceOf(account1), value);
+        vm.prank(account1);
+        simpleERC20.burn(value);
+        assertEq(simpleERC20.balanceOf(account1), 0);
+    }
+    function test_burnFrom() public {
+        test_mint();
+        
+        vm.prank(account1);
+        simpleERC20.approve(account3, value);
+        assertEq(simpleERC20.allowance(account1, account3), value);
+
+        
+        vm.prank(account3);
+        simpleERC20.burnFrom(account1, value);
+        
+        assertEq(simpleERC20.balanceOf(account1), 0);
+    }
+    // mint, burn
+}
